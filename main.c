e164d1e3a305a0d5200123bcbbaf0a140a9c84b0
Miguel Duarte <miduarte@c1r9s7.42lisboa.com>
2025-08-28 12:01:59 +0100

    vinicius

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: miduarte & adores <miduarte@student.42l    +#+  +:+       +#+        */
+/*   By: miduarte <miduarte@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 15:19:30 by miduarte          #+#    #+#             */
-/*   Updated: 2025/08/27 17:20:44 by miduarte &       ###   ########.fr       */
+/*   Updated: 2025/08/28 12:01:56 by miduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,68 +46,3 @@ void	shell_launch(char **args)
 }
 
 void shell_exec(char **args)
-{
-	int			i;
-	const char	*curr_builtin;
-
-	if (!args || !args[0])
-		return ;
-	i = 0;
-
-    //if is builtin
-        //run builtin
-	
-	// fork and launch 
-	shell_launch(args);
-}
-
-char *read_line(void)
-{
-    char *buf;
-    size_t bufsize;
-    char cwd[BUFSIZ];
-    
-    buf = NULL;
-    get_cwd(cwd, sizeof(cwd));
-    ft_printf(RED"%s "RST, cwd);
-    ft_printf(""C"minishell$ "RST);
-    if (getline(&buf, &bufsize, stdin) == -1)
-    {
-        if (feof(stdin))
-        {
-            ft_printf(RED"EOF\n"RST);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror(RED"readline"RST);
-            exit(EXIT_FAILURE);
-        }
-    }
-    return (buf);
-    
-}
-
-int main (int ac, char **av)
-{
-    char *line;
-    char **args;
-    int i;
-
-    (void)ac;
-    (void)av;
-    i = 0;
-    print_banner();
-    while((line = read_line()))
-    {   
-        args = split_line(line);
-        while(args[i])
-        {
-           ft_printf("You entered: %s\n", args[i]);            
-           i++;
-        }
-    }
-    free(line);
-    free(args);
-    return EXIT_SUCCESS;
-}
