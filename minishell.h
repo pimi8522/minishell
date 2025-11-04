2e061fa916a9d011e7bb0ff068ecb9da77b710ec
pimi8522 <pimi8522@gmail.com>
2025-11-04 11:53:52 +0000

    .h organizado

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: miduarte & adores <miduarte & adores@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 15:23:51 by miduarte &        #+#    #+#             */
-/*   Updated: 2025/11/04 11:02:36 by miduarte &       ###   ########.fr       */
+/*   Updated: 2025/11/04 11:51:42 by miduarte &       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -121,36 +121,96 @@ typedef struct s_shell
 #define RST 	"\1\033[0m\2"
 
 
+/*
+** srcs/utils.c
+*/
 char	*get_cwd(char *buf, size_t size);
 void	*do_malloc(size_t size);
 void	print_banner(void);
-char	**shell_split(char const *s);
 
-void	execute_command(char **args, char *envp[]);
-int		execute_single_builtin(t_cmd *cmd, t_shell *shell);
-void	execute_child(t_cmd *cmd, t_shell *shell, int input_fd, int pipe_fds[2]);
-int		exe_builtin(char **args, t_shell *shell);
+/*
+** srcs/utils/split.c
+*/
+char	**shell_split(char const *s);
+void	free_str_array(char **arr);
+void	free_str(char **str);
+
+/*
+** srcs/signals.c
+*/
 void	sigint_handler(int signo);
 void	setup_interactive_signals(void);
 void	setup_exec_signals(void);
 void	heredoc_sigint_handler(int signo);
+
+/*
+** srcs/history.c
+*/
 int		init_shell_history(void);
 void	save_shell_history(void);
+
+/*
+** srcs/env/env.c
+*/
 t_env	*init_env(char **envp);
 char	**convert_env_to_array(t_env *env_list);
 t_env	*find_env_node(t_env *env_list, const char *key);
 void	add_env_node_back(t_env **env_list_head, t_env *new_node);
 t_env	*new_env_node(char *key, char *value);
-void	bubble_sort_array(char **arr);
-void	free_str(char **str);
 void	free_env_node(t_env *node);
-void free_env_list(t_env *head);
+void	free_env_list(t_env *head);
+
+/*
+** srcs/env/env_utils.c
+*/
+void	bubble_sort_array(char **arr);
+void	set_env_var(t_shell *shell, const char *key, const char *value);
+char	*get_env_value(t_shell *shell, const char *key);
+int		is_valid_identifier(const char *str);
+
+/*
+** srcs/lexer/lexer.c
+*/
+t_token	*lexer(char const *s);
+void	ft_clear_token_list(t_token **list);
+int		is_separator_char(char c);
+void	skip_whitespace(const char *s, size_t *i);
+int		is_quote(char c);
+bool	ft_skip_quotes(const char *line, size_t *i);
+void	ft_print_quote_err(char c);
+
+/*
+** srcs/parser/parser.c
+*/
 t_cmd	*parser(char *line, t_shell *shell);
-void	free_cmds(t_cmd *cmd_list);
+t_cmd	*parse_line(char *line, t_shell *shell);
 t_cmd	*new_cmd_node(char *cmd, char **flags);
 void	add_cmd_node_back(t_cmd **cmd_list_head, t_cmd *new_node);
+void	free_cmds(t_cmd *cmd_list);
+
+/*
+** srcs/heredoc/heredoc.c
+*/
 int		handle_heredoc(const char *delimiter, int expand, t_shell *shell);
 
+/*
+** srcs/expansion/expand.c
+*/
+void	expand_variables(t_cmd *cmd, t_shell *shell);
+char	*expand_line_for_heredoc(char *line, t_shell *shell);
+
+/*
+** srcs/exec/exec.c
+*/
+int		execute_pipeline(t_cmd *cmds, t_shell *shell);
+void	execute_child(t_cmd *cmd, t_shell *shell, int input_fd, int pipe_fds[2]);
+void	execute_command(char **args, char *envp[]);
+int		execute_single_builtin(t_cmd *cmd, t_shell *shell);
+int		exe_builtin(char **args, t_shell *shell);
+
+/*
+** srcs/builtins/*.c
+*/
 int		echo_builtin(char **args, t_shell *shell);
 int		ft_cd(char **args, t_shell *shell);
 int		pwd_builtin(t_shell *shell);
@@ -161,31 +221,5 @@ int		unset_builtin(char **args, t_shell *shell);
 int		export_builtin(char **args, t_shell *shell);
 int		is_builtin(char **args);
 char	*get_target_path(char **args, t_shell *shell);
-
-void	set_env_var(t_shell *shell, const char *key, const char *value);
-char	*get_env_value(t_shell *shell, const char *key);
-void	expand_variables(t_cmd *cmd, t_shell *shell);
-char	*expand_line_for_heredoc(char *line, t_shell *shell);
-int		execute_pipeline(t_cmd *cmds, t_shell *shell);
-int		is_valid_identifier(const char *str);
-
-/*
-** Lexer
-*/
-t_token	*lexer(char const *s);
-void	ft_clear_token_list(t_token **list);
-int		is_separator_char(char c);
-void	skip_whitespace(const char *s, size_t *i);
-int		is_quote(char c);
-bool	ft_skip_quotes(const char *line, size_t *i);
-void	ft_print_quote_err(char c);
-void	free_str_array(char **arr);
-
-/*
-** Parser
-*/
-t_cmd	*parse_line(char *line, t_shell *shell);
-void	free_cmds(t_cmd *cmd_list);
-void	add_cmd_node_back(t_cmd **cmd_list_head, t_cmd *new_node);
 
 #endif
